28f6cb0d5017aa6a697015008a68196ae67a647b
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-03-11 09:46:50 -0300

    chore: update linked lists revisited until get

--- pyds/linked_list_revisited.py
+++ pyds/linked_list_revisited.py
@@ -1,3 +1,10 @@
+import logging
+
+
+logging.basicConfig(encoding="utf-8", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -11,11 +18,17 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
+    def log_list(self):
+        items = []
+        current = self.head
+        while current is not None:
+            items.append(current.value)
+            current = current.next
+
+        logger.info(f"\n{items}")
+
+    def get_length(self):
+        logger.info(f"\n{self.length}")
 
     def append(self, value):
         # Create new node
@@ -46,18 +59,69 @@ class LinkedList:
             self.length -= 1
             return
 
-        # More items case
-        temp = self.head
-        pre = self.head
-        while temp.next:
-            pre = temp
-            temp = temp.next
+        # More than one node case
+        current_node = self.head
+        node_before = self.head
+        while current_node.next:
+            node_before = current_node
+            current_node = current_node.next
 
-        self.tail = pre
+        self.tail = node_before
         self.tail.next = None
         self.length -= 1
 
+    def prepend(self, value):
+        new_node = Node(value)
+
+        # Empty case
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+            return
+
+        # More nodes case
+        old_head_node = self.head
+        self.head = new_node
+        new_node.next = old_head_node
+        self.length += 1
+
+    def pop_first(self):
+        # Empty case
+        if self.length == 0:
+            return
+
+        # One node case
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return
+
+        # More nodes case
+        old_head = self.head
+        self.head = self.head.next
+        old_head.next = None
+        self.length -= 1
+
+    # This is not an index like in arrays, here we simulate an index by traversing
+    # the linked list using a variable that corresponds to the number of iteractions
+    # that we will need to do
+    def get_by_index(self, index):
+        if not isinstance(index, int) or index < 0 or index >= self.length:
+            logger.info("\nProvide a positive integer starting from 0.")
+            return
+
+        current_node = self.head
+        for _ in range(index):
+            current_node = current_node.next
+
+        logger.info(f"\n{current_node.value}")
+
 
 test = LinkedList(5)
-test.pop()
-test.print_list()
+test.append(4)
+test.append(3)
+test.append(2)
+test.append(1)
+test.get_by_index(1)
