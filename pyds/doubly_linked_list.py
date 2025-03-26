b4b74ad62912d3e565c8094c1eb2424753599940
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-03-26 08:54:12 -0300

    chore: add constructor for dll

---
+++ pyds/doubly_linked_list.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.previous = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+
+dll = DoublyLinkedList(4)
+dll.print_list()
