d231d0ce3924f8f8e715f36725d50172faca0efb
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-03-27 09:01:43 -0300

    chore: add append_node to dll

--- pyds/doubly_linked_list.py
+++ pyds/doubly_linked_list.py
@@ -18,6 +18,24 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
 
+    def append_node(self, value):
+        new_node = Node(value)
+
+        # Empty DLL. Could also check if self.head is None
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+            return
+
+        # One or more nodes
+        self.tail.next = new_node
+        new_node.next = None
+        new_node.previous = self.tail
+        self.length += 1
+        return
+
 
 dll = DoublyLinkedList(4)
+dll.append_node(5)
 dll.print_list()
